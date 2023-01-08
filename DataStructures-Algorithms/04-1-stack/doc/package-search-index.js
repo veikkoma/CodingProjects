6b389b79a0b4ab04d5c0e7bd430aac23fbf47709
Matias Veikkola <55023796+veikkoma@users.noreply.github.com>
2023-01-08 18:41:14 +0200

    Add files via upload

---
+++ DataStructures-Algorithms/04-1-stack/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"oy.tol.tra"}];updateSearchResults();
---
+++ DataStructures-Algorithms/04-1-stack/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"oy.tol.tra","l":"ParenthesisChecker"},{"p":"oy.tol.tra","l":"StackAllocationException"},{"p":"oy.tol.tra","l":"StackFactory"},{"p":"oy.tol.tra","l":"StackImplementation"},{"p":"oy.tol.tra","l":"StackInterface"},{"p":"oy.tol.tra","l":"StackIsEmptyException"}];updateSearchResults();
